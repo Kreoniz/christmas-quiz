dbe587ae4a25ec42e16348c2181c5292eea7a423
Kreoniz <kreoniz@mail.ru>
2023-12-25 17:07:37 +0600

    Extract random cat function into layout

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,5 +1,11 @@
+function getRandomInt(min, max) {
+	return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
 export function load({ url }) {
+	const randomCat = 'cat' + getRandomInt(1, 4);
 	return {
-		url: url.pathname
+		url: url.pathname,
+		cat: randomCat
 	};
 }
