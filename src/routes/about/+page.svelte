c6a9ffbfb14541083290f1e4ba52a22abc55fa3a
Kreoniz <kreoniz@mail.ru>
2023-12-25 20:16:41 +0600

    Add content to about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,5 +1,62 @@
+<script>
+  import svelteIcon from '$lib/img/svelte-logo-square.svg';
+  import MagicalText from '$lib/MagicalText.svelte';
+</script>
+
 <svelte:head>
 	<title>About</title>
 </svelte:head>
 
-<h1>I'm an about page!</h1>
+<div class="container">
+  <h1>Long story short...</h1>
+  <p class="info centered">This practice project is built with <span class="bold"><MagicalText text="love!" /></span></p>
+  <a href="https://kit.svelte.dev/" class="svelte">
+    <img class="svelte" src={svelteIcon} alt="svelte logo">
+  </a>
+  <p>...And with <a class="link" href="https://kit.svelte.dev/">SvelteKit!</a> An awesome framework enabling anyone to write complex web applications!</p>
+</div>
+
+<style>
+  .container {
+    padding: 0 1rem;
+  }
+
+  h1 {
+    font-size: 1.8rem;
+    line-height: 1.5;
+    font-weight: 600;
+  }
+
+  p {
+    margin-block: 8px;
+    line-height: 1.2;
+  }
+
+  .info {
+    font-size: 1.5rem;
+  }
+
+  a:hover {
+    cursor: pointer;
+  }
+
+  a.svelte {
+    display: block;
+    width: 150px;
+    margin: 5px auto;
+  }
+
+  a.link {
+    color: var(--text);
+    text-decoration: underline;
+    transition: var(--background-transition);
+  }
+
+  .bold {
+    font-weight: 600;
+  }
+
+  .centered {
+    text-align: center;
+  }
+</style>
